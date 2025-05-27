8f42c120332615e69ee5939db145fc024b158d42
Samusus27 <samueloviedo2797@gmail.com>
2025-05-26 18:05:34 -0600

    Primera versión de la calculadora Flask

---
+++ Clases_Progra1/Ejemplo.py
@@ -0,0 +1,12 @@
+n = int(input("Ingrese el valor de n "))
+suma = (n * (n + 1)) // 2
+print(suma)
+
+k = input("Ingrese el valor de k ")
+
+if k > 0:
+    print("Positivo")
+elif k < 0:
+    print("Positivo")
+else:
+    print(0)
